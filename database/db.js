4c18b689ed9f7a88ee88925aef61320d83ebef98
David435412 <davidm435465123@gmail.com>
2024-11-21 08:02:58 -0500

    cambios en las url y seguridad

--- database/db.js
+++ database/db.js
@@ -1,9 +1,29 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('proyecto_Mila', 'root', '',{
 
-    host:'localhost',
-    dialect:'mysql'
-})
+dotenv.config();
+
+
+const db = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER,  
+  process.env.DB_PASSWORD,  
+  {
+    host: process.env.DB_HOST,  
+    dialect: process.env.DB_DIALECT,  
+    port: process.env.DB_PORT,  
+  }
+);
+
+
+db.authenticate()
+  .then(() => {
+    console.log('Conexión establecida con éxito.');
+  })
+  .catch((error) => {
+    console.error('No se pudo conectar a la base de datos:', error);
+  });
+
 
 export default db;
